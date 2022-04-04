647b2573f35701fc080e180929b9aec7b0793716
Israel Alonso <93823993+isAlon85@users.noreply.github.com>
2022-04-04 22:35:16 +0200

    datos

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY ,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN ,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID ,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET ,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID ,
-  appId: process.env.REACT_APP_FIREBASE_APPID ,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+  apiKey: "AIzaSyCCzRNF1-AmX9tDzbmtDgxEILitHDkufy4",
+  authDomain: "pruebafire-77451.firebaseapp.com",
+  projectId: "pruebafire-77451",
+  storageBucket: "pruebafire-77451.appspot.com",
+  messagingSenderId: "602225838796",
+  appId: "1:602225838796:web:7bce051146b7f8fc569069",
+  measurementId: "G-YBDWP40DBB"
 };
 
 // Initialize Firebase
